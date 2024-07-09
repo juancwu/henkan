b616480aff39c2dfda10e5a9cfaaea2df95169dd
jc <46619361+juancwu@users.noreply.github.com>
2024-07-09 13:40:32 -0400

    remove temperature module
    removing it because it doesn't make sense to have it there without a way to understand user input yet

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::io::{self, BufRead, Write};
 
-mod temperature;
-
 fn main() {
     println!("Welcome to Henkan, an ergonomic unit converter in the terminal.");
-    println!("Try entering 28c in f");
     print!("> ");
     io::stdout().flush().unwrap();
     let stdin = io::stdin();
