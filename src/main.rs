f723a949fb056be43df61f3d2a46d9e5991b12db
jc <46619361+juancwu@users.noreply.github.com>
2024-07-10 16:27:24 -0400

    minimal expression parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::io::{self, BufRead, Write};
-
-use crate::{lexer::Lexer, token::Token};
-
 // NOTE: `mod` declares the module available to the compiler.
 mod lexer;
+mod parser;
 mod token;
+
+use std::io::{self, BufRead, Write};
+
+use crate::{
+    lexer::Lexer,
+    parser::{parse_expression, ASTNode},
+    token::Token,
+};
 
 fn main() {
     println!("Welcome to Henkan, an ergonomic unit converter in the terminal.");
@@ -14,13 +19,56 @@ fn main() {
     for line in stdin.lock().lines() {
         let l = line.unwrap();
         let mut lexer = Lexer::new(l);
-        let mut token: Token;
+        let mut tokens: Vec<Token> = vec![];
         while !lexer.is_end() {
-            token = lexer.next_token();
+            let token = lexer.next_token();
+            tokens.push(token);
             if token == Token::EOL {
                 break;
             }
             println!("token: {:?}", token);
+        }
+        let ast = parse_expression(&tokens);
+        if ast.is_some() {
+            let node = ast.unwrap();
+            match node {
+                ASTNode::Expr {
+                    value,
+                    value_unit,
+                    operator,
+                    unit,
+                } => {
+                    let n = match value {
+                        Token::Number(v) => v,
+                        _ => 0.0,
+                    };
+                    let n_u = match value_unit {
+                        Token::Centimeter => "cm",
+                        Token::Millimeter => "mm",
+                        Token::Meter => "m",
+                        Token::Kilometer => "km",
+                        Token::Celcius => "Celsius",
+                        Token::Fahrenheit => "Fahrenheit",
+                        _ => "",
+                    };
+                    let op = match operator {
+                        Token::To => "to",
+                        Token::As => "as",
+                        Token::In => "in",
+                        _ => "",
+                    };
+                    let u = match unit {
+                        Token::Centimeter => "cm",
+                        Token::Millimeter => "mm",
+                        Token::Meter => "m",
+                        Token::Kilometer => "km",
+                        Token::Celcius => "Celsius",
+                        Token::Fahrenheit => "Fahrenheit",
+                        _ => "",
+                    };
+                    println!("ast: {}{} {} {}", n, n_u, op, u);
+                }
+            }
         }
         print!("> ");
         io::stdout().flush().unwrap();
---
+++ src/parser.rs
@@ -0,0 +1,96 @@
+use crate::token::Token;
+use std::fmt::{self, write};
+
+pub enum ASTNode {
+    Expr {
+        value: Token,
+        value_unit: Token,
+        operator: Token,
+        unit: Token,
+    },
+}
+
+impl fmt::Display for ASTNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ASTNode")
+    }
+}
+
+/// This is a simple parsing function to only parse expressions.
+/// Only for getting the parsing started.
+pub fn parse_expression(tokens: &[Token]) -> Option<ASTNode> {
+    if tokens.is_empty() || tokens.len() < 4 {
+        return None;
+    }
+
+    let left = if let Token::Number(v) = tokens[0] {
+        Token::Number(v)
+    } else {
+        return None;
+    };
+
+    let left_unit = match match_unit(&tokens[1]) {
+        Some(u) => u,
+        None => return None,
+    };
+
+    let op = match match_op(&tokens[2]) {
+        Some(op) => op,
+        None => return None,
+    };
+
+    let right_unit = match match_unit(&tokens[3]) {
+        Some(u) => u,
+        None => return None,
+    };
+
+    Some(ASTNode::Expr {
+        value: left,
+        value_unit: left_unit,
+        operator: op,
+        unit: right_unit,
+    })
+}
+
+/// Tries to match a unit in the given token.
+/// # Example
+/// ````
+/// let unit = match match_unit(token) {
+///     Some(t) => t,
+///     None => None,
+/// };
+/// ````
+fn match_unit(token: &Token) -> Option<Token> {
+    let t = match token {
+        Token::Centimeter => Token::Centimeter,
+        Token::Millimeter => Token::Millimeter,
+        Token::Meter => Token::Meter,
+        Token::Kilometer => Token::Kilometer,
+        Token::Celcius => Token::Celcius,
+        Token::Fahrenheit => Token::Fahrenheit,
+        _ => {
+            return None;
+        }
+    };
+    Some(t)
+}
+
+/// Tries to match an operator token for the given token.
+/// # Example
+/// ````
+/// let op = match_op(token) {
+///     Some(t) => t,
+///     None => None,
+/// }
+/// ````
+fn match_op(token: &Token) -> Option<Token> {
+    let op = match token {
+        Token::To => Token::To,
+        Token::In => Token::In,
+        Token::As => Token::As,
+        _ => {
+            return None;
+        }
+    };
+    Some(op)
+}
